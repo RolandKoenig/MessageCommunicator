69c51ab87a837c0ca6a7f97d26040ee5a7cbf15a
Roland König <roland.koenig@rolandk.de>
2020-07-02 18:16:09 +0200

    Some tests for endsymbol recognition

--- TcpCommunicator.Tests/EndSymbolRecognitionTests.cs
+++ TcpCommunicator.Tests/EndSymbolRecognitionTests.cs
@@ -41,5 +41,40 @@ namespace TcpCommunicator.Tests
             Assert.IsTrue(receivedMessages[0].RawMessage.ToString() == "Test1");
             Assert.IsTrue(receivedMessages[1].RawMessage.ToString() == "Test2");
         }
+
+        [TestMethod]
+        public void Test_EndSymbols_SpecialCase_TwoParts()
+        {
+            var tcpCommunicatorMock = A.Fake<ITcpCommunicator>();
+            
+            var messageRecognizer = new EndSymbolMessageRecognizer(
+                tcpCommunicatorMock,
+                Encoding.Unicode, new char[]{'#', '#'});
+
+            var receivedMessages = new List<Message>(2);
+            messageRecognizer.ReceiveHandler = (msg) => receivedMessages.Add(msg);
+
+            tcpCommunicatorMock.ReceiveHandler.Invoke(Encoding.Unicode.GetBytes("Tes"));
+            tcpCommunicatorMock.ReceiveHandler.Invoke(Encoding.Unicode.GetBytes("t##"));
+            Assert.IsTrue(receivedMessages[0].RawMessage.ToString() == "Test");
+        }
+
+        [TestMethod]
+        public void Test_EndSymbols_SpecialCase_CuttedEndSymbol()
+        {
+            var tcpCommunicatorMock = A.Fake<ITcpCommunicator>();
+            
+            var messageRecognizer = new EndSymbolMessageRecognizer(
+                tcpCommunicatorMock,
+                Encoding.Unicode, new char[]{'#', '#'});
+
+            var receivedMessages = new List<Message>(2);
+            messageRecognizer.ReceiveHandler = (msg) => receivedMessages.Add(msg);
+
+            tcpCommunicatorMock.ReceiveHandler.Invoke(Encoding.Unicode.GetBytes("Tes"));
+            tcpCommunicatorMock.ReceiveHandler.Invoke(Encoding.Unicode.GetBytes("t#"));
+            tcpCommunicatorMock.ReceiveHandler.Invoke(Encoding.Unicode.GetBytes("#"));
+            Assert.IsTrue(receivedMessages[0].RawMessage.ToString() == "Test");
+        }
     }
 }
