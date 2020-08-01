73afd402334bd3b681d880f64b65beef65a41cef
Roland König <roland.koenig@rolandk.de>
2020-08-01 14:03:08 +0200

    Visualize LoggingMessageType by color

---
+++ MessageCommunicator.TestGui/_Util/_Converters/LoggingMessageTypeToColorConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+namespace MessageCommunicator.TestGui
+{
+    public class LoggingMessageTypeToColorConverter : IValueConverter
+    {
+        /// <inheritdoc />
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var connState = (string) value;
+            switch (connState)
+            {
+                case nameof(LoggingMessageType.Error):
+                    return Brushes.Red;
+
+                case nameof(LoggingMessageType.Warning):
+                    return Brushes.Yellow;
+                
+                default:
+                    return Brushes.Transparent;
+            }
+        }
+
+        /// <inheritdoc />
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
