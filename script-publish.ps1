0fd6847fa572f1315bca9874bc011088705d26cd
Roland König <roland.koenig@rolandk.de>
2020-08-14 06:11:51 +0200

    Added macOS as publish target

--- script-publish.ps1
+++ script-publish.ps1
@@ -7,30 +7,39 @@ dotnet pack -c Release -o ./publish ./MessageCommunicator
 dotnet publish -c Release -f netcoreapp3.1 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true --runtime win-x86 -o "./publish/MessageCommunicator (Win X86)" ./MessageCommunicator.TestGui
 dotnet publish -c Release -f netcoreapp3.1 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true --runtime win-x64 -o "./publish/MessageCommunicator (Win X64)" ./MessageCommunicator.TestGui
 dotnet publish -c Release -f netcoreapp3.1 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true --runtime linux-x64 -o "./publish/MessageCommunicator (Linux X64)" ./MessageCommunicator.TestGui
+dotnet publish -c Release -f netcoreapp3.1 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true --runtime osx-x64 -o "./publish/MessageCommunicator (macOS 10.12)" ./MessageCommunicator.TestGui
 
 # Remove pdb files
 Copy-Item "./publish/MessageCommunicator (Win X86)/MessageCommunicator.TestGui.exe" -Destination "./publish/MessageCommunicator-Win-X86.exe"
 Copy-Item "./publish/MessageCommunicator (Win X64)/MessageCommunicator.TestGui.exe" -Destination "./publish/MessageCommunicator-Win-X64.exe"
 Copy-Item "./publish/MessageCommunicator (Linux X64)/MessageCommunicator.TestGui" -Destination "./publish/MessageCommunicator-Linux-X64"
+Copy-Item "./publish/MessageCommunicator (macOS 10.12)/MessageCommunicator.TestGui" -Destination "./publish/MessageCommunicator-macOS-X64"
 
 # Compress each build for lower disk usage
 $compress = @{
   Path = "./publish/MessageCommunicator-Win-X86.exe"
   CompressionLevel = "Optimal"
-  DestinationPath = ".publish/MessageCommunicator-Win-X86.zip"
+  DestinationPath = "./publish/MessageCommunicator-Win-X86.zip"
 }
 Compress-Archive @compress
 
 $compress = @{
   Path = "./publish/MessageCommunicator-Win-X64.exe"
   CompressionLevel = "Optimal"
-  DestinationPath = ".publish/MessageCommunicator-Win-X64.zip"
+  DestinationPath = "./publish/MessageCommunicator-Win-X64.zip"
 }
 Compress-Archive @compress
 
 $compress = @{
   Path = "./publish/MessageCommunicator-Linux-X64"
   CompressionLevel = "Optimal"
-  DestinationPath = ".publish/MessageCommunicator-Linux-X64.zip"
+  DestinationPath = "./publish/MessageCommunicator-Linux-X64.zip"
+}
+Compress-Archive @compress
+
+$compress = @{
+  Path = "./publish/MessageCommunicator-macOS-X64"
+  CompressionLevel = "Optimal"
+  DestinationPath = "./publish/MessageCommunicator-macOS-X64.zip"
 }
 Compress-Archive @compress
