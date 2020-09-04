76ecfcf9dae1b481f194d59aa5b65a0d3db6fb6a
Roland König <roland.koenig@rolandk.de>
2020-09-04 13:59:44 +0200

    Preparing theme changed (light / dark)

--- script-publish.ps1
+++ script-publish.ps1
@@ -7,13 +7,13 @@ dotnet pack -c Release -o ./publish ./MessageCommunicator
 dotnet publish -c Release -f netcoreapp3.1 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true --runtime win-x86 -o "./publish/MessageCommunicator (Win X86)" ./MessageCommunicator.TestGui
 dotnet publish -c Release -f netcoreapp3.1 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true --runtime win-x64 -o "./publish/MessageCommunicator (Win X64)" ./MessageCommunicator.TestGui
 dotnet publish -c Release -f netcoreapp3.1 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true --runtime linux-x64 -o "./publish/MessageCommunicator (Linux X64)" ./MessageCommunicator.TestGui
-dotnet publish -c Release -f netcoreapp3.1 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true --runtime osx-x64 -o "./publish/MessageCommunicator (macOS 10.12)" ./MessageCommunicator.TestGui
+dotnet publish -c Release -f netcoreapp3.1 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true --runtime osx-x64 -o "./publish/MessageCommunicator (macOS 10.12 X64)" ./MessageCommunicator.TestGui
 
 # Remove pdb files
 Copy-Item "./publish/MessageCommunicator (Win X86)/MessageCommunicator.TestGui.exe" -Destination "./publish/MessageCommunicator-Win-X86.exe"
 Copy-Item "./publish/MessageCommunicator (Win X64)/MessageCommunicator.TestGui.exe" -Destination "./publish/MessageCommunicator-Win-X64.exe"
 Copy-Item "./publish/MessageCommunicator (Linux X64)/MessageCommunicator.TestGui" -Destination "./publish/MessageCommunicator-Linux-X64"
-Copy-Item "./publish/MessageCommunicator (macOS 10.12)/MessageCommunicator.TestGui" -Destination "./publish/MessageCommunicator-macOS-X64"
+Copy-Item "./publish/MessageCommunicator (macOS 10.12 X64)/MessageCommunicator.TestGui" -Destination "./publish/MessageCommunicator-macOS-X64"
 
 # Compress each build for lower disk usage
 $compress = @{
