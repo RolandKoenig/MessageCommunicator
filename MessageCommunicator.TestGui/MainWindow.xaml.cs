6ad0e27d2a8e78543547a63034d6dde0b344254a
Roland König <roland.koenig@rolandk.de>
2020-09-07 07:48:05 +0200

    Switching theme between light and dark

--- MessageCommunicator.TestGui/MainWindow.xaml.cs
+++ MessageCommunicator.TestGui/MainWindow.xaml.cs
@@ -12,9 +12,13 @@ namespace MessageCommunicator.TestGui
 {
     public class MainWindow : OwnWindow<MainWindowViewModel>
     {
+        private Grid _ctrlMainGrid;
+
         public MainWindow()
         {
             AvaloniaXamlLoader.Load(this);
+
+            _ctrlMainGrid = this.FindControl<Grid>("CtrlMainGrid");
 
             var versionString = Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "";
             this.Title = $"{this.Title} {versionString}";
@@ -43,11 +47,19 @@ namespace MessageCommunicator.TestGui
         private void OnMnuThemeLight_PointerPressed(object sender, PointerPressedEventArgs eArgs)
         {
             this.Styles[0] = (StyleInclude)this.Resources["ThemeLight"];
+
+            _ctrlMainGrid.IsVisible = false;
+            _ctrlMainGrid.IsVisible = true;
         }
 
         private void OnMnuThemeDark_PointerPressed(object sender, PointerPressedEventArgs eArgs)
         {
             this.Styles[0] = (StyleInclude)this.Resources["ThemeDark"];
+
+            _ctrlMainGrid.IsVisible = false;
+            _ctrlMainGrid.IsVisible = true;
+
+            
         }
     }
 }
