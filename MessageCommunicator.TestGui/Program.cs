5114ea09e71a60235b330513bfcedc053f513717
Roland König <roland.koenig@rolandk.de>
2020-11-08 09:18:38 +0100

    Prepared windows store publishing

--- MessageCommunicator.TestGui/Program.cs
+++ MessageCommunicator.TestGui/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using Avalonia;
 using Avalonia.Logging.Serilog;
 using Avalonia.ReactiveUI;
@@ -15,6 +16,11 @@ namespace MessageCommunicator.TestGui
         {
             try
             {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    StartupWindowsSettings();
+                }
+
                 BuildAvaloniaApp()
                     .StartWithClassicDesktopLifetime(args);
             }
@@ -33,6 +39,27 @@ namespace MessageCommunicator.TestGui
                 .UsePlatformDetect()
                 .UseReactiveUI()
                 .LogToDebug();
+        }
+
+        public static void StartupWindowsSettings()
+        {
+            // Code from https://stackoverflow.com/questions/43537990/wpf-clickonce-dpi-awareness-per-monitor-v2
+
+            if (Environment.OSVersion.Version >= new Version(6, 3, 0)) // win 8.1 added support for per monitor dpi
+            {
+                if (Environment.OSVersion.Version >= new Version(10, 0, 15063)) // win 10 creators update added support for per monitor v2
+                {
+                    NativeMethodsWin32.SetProcessDpiAwarenessContext((int)NativeMethodsWin32.DPI_AWARENESS_CONTEXT.DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+                }
+                else
+                {
+                    NativeMethodsWin32.SetProcessDpiAwareness(NativeMethodsWin32.PROCESS_DPI_AWARENESS.Process_Per_Monitor_DPI_Aware);
+                }
+            }
+            else
+            {
+                NativeMethodsWin32.SetProcessDPIAware();
+            }
         }
     }
 }
