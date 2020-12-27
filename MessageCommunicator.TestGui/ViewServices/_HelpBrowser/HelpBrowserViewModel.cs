5abc6d340dd3ffa838e0cd2ae6191ef1d647bf23
Roland König <roland.koenig@rolandk.de>
2020-12-27 17:28:40 +0100

    Added list for all help files in HelpBrowser

--- MessageCommunicator.TestGui/ViewServices/_HelpBrowser/HelpBrowserViewModel.cs
+++ MessageCommunicator.TestGui/ViewServices/_HelpBrowser/HelpBrowserViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Reactive;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reactive;
 using ReactiveUI;
 
 namespace MessageCommunicator.TestGui.ViewServices
@@ -6,30 +8,43 @@ namespace MessageCommunicator.TestGui.ViewServices
     public class HelpBrowserViewModel : OwnViewModelBase
     {
         private IntegratedDocRepository _docRepo;
+        private IntegratedDocFile? _currentDocFile;
+
+        public string LoadedDoc => _currentDocFile == null ? "" : _currentDocFile.ReadFullContent();
+
+        public string WindowTitle =>
+            _currentDocFile == null ? "Help Browser" : $"Help Browser - {_currentDocFile.Title}";
+
+        public IEnumerable<IntegratedDocFile> AllFiles => _docRepo.AllFiles;
         
-        public string LoadedDoc { get; private set; }
-        
-        public string WindowTitle { get; private set; }
+        public IntegratedDocFile? CurrentDocFile
+        {
+            get => _currentDocFile;
+            private set
+            {
+                if (_currentDocFile != value)
+                {
+                    _currentDocFile = value;
+                    this.RaisePropertyChanged();
+                    this.RaisePropertyChanged(nameof(this.LoadedDoc));
+                    this.RaisePropertyChanged(nameof(this.WindowTitle));
+                }
+            }
+        }
         
         public ReactiveCommand<object?, Unit> Command_Close { get; }
         
         public HelpBrowserViewModel(IntegratedDocRepository docRepo)
         {
             _docRepo = docRepo;
-            this.LoadedDoc = string.Empty;
-            this.WindowTitle = "Help Browser";
-            
+
             this.Command_Close = ReactiveCommand.Create<object?>(
                 arg => this.CloseWindow(null));
         }
 
         public void NavigateTo(string docFileTitle)
         {
-            this.LoadedDoc = _docRepo.GetByTitle(docFileTitle).ReadFullContent();
-            this.RaisePropertyChanged(nameof(this.LoadedDoc));
-
-            this.WindowTitle = $"Help Browser - {docFileTitle}";
-            this.RaisePropertyChanged(nameof(this.WindowTitle));
+            this.CurrentDocFile = _docRepo.GetByTitle(docFileTitle);
         }
     }
 }
