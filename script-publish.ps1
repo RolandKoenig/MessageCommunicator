a54de4f56fec91201cc12bed7e324822d06a598a
Roland König <roland.koenig@rolandk.de>
2021-01-05 06:47:56 +0100

    Updated publish scripts

--- script-publish.ps1
+++ script-publish.ps1
@@ -8,7 +8,7 @@ dotnet pack -c Release -o ./publish ./MessageCommunicator.SerialPorts /p:Continu
 dotnet publish -c Release -f net5.0 --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishTrimmed=true -p:TrimMode=Link --runtime win-x86 -o "./publish/MessageCommunicator (Win X86)" ./MessageCommunicator.TestGui
 dotnet publish -c Release -f net5.0 --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishTrimmed=true -p:TrimMode=Link --runtime win-x64 -o "./publish/MessageCommunicator (Win X64)" ./MessageCommunicator.TestGui
 dotnet publish -c Release -f net5.0 --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishTrimmed=true -p:TrimMode=Link --runtime linux-x64 -o "./publish/MessageCommunicator (Linux X64)" ./MessageCommunicator.TestGui
-dotnet publish -c Release -f net5.0 --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishTrimmed=true -p:TrimMode=Link --runtime osx-x64 -o "./publish/MessageCommunicator (macOS 10.12 X64)" ./MessageCommunicator.TestGui
+dotnet publish -c Release -f net5.0 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=Link --runtime osx-x64 -o "./publish/MessageCommunicator (macOS 10.12 X64)" ./MessageCommunicator.TestGui
 
 # Remove pdb files
 Copy-Item "./publish/MessageCommunicator (Win X86)/MessageCommunicator.TestGui.exe" -Destination "./publish/MessageCommunicator-Win-X86.exe"
@@ -39,7 +39,7 @@ $compress = @{
 Compress-Archive @compress
 
 $compress = @{
-  Path = "./publish/MessageCommunicator-macOS-X64"
+  Path = "./publish/MessageCommunicator-macOS-X64", "./publish/MessageCommunicator (macOS 10.12 X64)/*.dylib"
   CompressionLevel = "Optimal"
   DestinationPath = "./publish/MessageCommunicator-macOS-X64.zip"
 }
