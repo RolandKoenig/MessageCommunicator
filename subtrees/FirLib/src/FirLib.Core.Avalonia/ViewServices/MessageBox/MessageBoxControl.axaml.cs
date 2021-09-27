48eb576eb303664d9c0cf01e620edd7ab59865f1
Roland König <roland.koenig@rolandk.de>
2021-09-27 17:29:58 +0200

    Added OpenFileDialogService and SaveFileDialogService for FirLib.Core.Avalonia

---
+++ subtrees/FirLib/src/FirLib.Core.Avalonia/ViewServices/MessageBox/MessageBoxControl.axaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using FirLib.Core.Avalonia.Controls;
+
+// Simple message box implementation
+// Based on: https://stackoverflow.com/questions/55706291/how-to-show-a-message-box-in-avaloniaui-beta
+
+namespace FirLib.Core.ViewServices.MessageBox
+{
+    public class MessageBoxControl : UserControl
+    {
+        public MessageBoxControl()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public static Task<MessageBoxResult> ShowAsync(DialogHostControl host, string title, string text, MessageBoxButtons buttons)
+        {
+            var tcs = new TaskCompletionSource<MessageBoxResult>();
+
+            var msgbox = new MessageBoxControl();
+            msgbox.FindControl<TextBlock>("Text").Text = text;
+            var buttonPanel = msgbox.FindControl<StackPanel>("Buttons");
+
+            var res = MessageBoxResult.Ok;
+
+            void AddButton(string caption, MessageBoxResult r, bool def = false)
+            {
+                var btn = new Button {Content = caption};
+                btn.Click += (_, __) => { 
+                    res = r;
+                    host.CloseDialog();
+                    tcs.TrySetResult(res);
+                };
+                buttonPanel.Children.Add(btn);
+                if (def)
+                {
+                    res = r;
+                }
+            }
+
+            switch (buttons)
+            {
+                case MessageBoxButtons.Ok:
+                case MessageBoxButtons.OkCancel:
+                    AddButton("Ok", MessageBoxResult.Ok, true);
+                    break;
+
+                case MessageBoxButtons.YesNo:
+                case MessageBoxButtons.YesNoCancel:
+                    AddButton("Yes", MessageBoxResult.Yes, true);
+                    AddButton("No", MessageBoxResult.No);
+                    break;
+            }
+
+            if (buttons == MessageBoxButtons.OkCancel || buttons == MessageBoxButtons.YesNoCancel)
+            {
+                AddButton("Cancel", MessageBoxResult.Cancel, true);
+            }
+
+            host.ShowDialog(msgbox, title);
+
+            return tcs.Task;
+        }
+    }
+}
---
+++ subtrees/FirLib/src/FirLib.Core/ViewServices/ISaveFileDialogService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using FirLib.Core.Patterns.Mvvm;
+
+namespace FirLib.Core.ViewServices
+{
+    public interface ISaveFileViewService : IViewService
+    {
+        Task<string?> ShowSaveFileDialogAsync(IEnumerable<FileDialogFilter> filters, string defaultExtension);
+    }
+}
